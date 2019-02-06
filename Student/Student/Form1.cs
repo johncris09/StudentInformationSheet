49fd0095ed122c9df2c7748bda3f337746fcb77a
Johncris <jcmanabo@gmail.com>
2019-02-07 00:12:50 +0800

    edit Record

--- Student/Student/Form1.cs
+++ Student/Student/Form1.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.Data.OleDb;
+using System.Globalization;
+ 
+
+using System.Threading; 
 
 namespace Student
 {
@@ -68,35 +72,100 @@ namespace Student
             MessageBox.Show("Record Inserted Successfully", "Student Information", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
             SelectAll();
             IDGenerator();
+            ClearAll();
 
         }
 
         public void ClearAll()
         {
-            student_IDTextBox.Text = "";
-            Last_NameTextBox.Text = "";
-            student_IDTextBox.Text = "";
+            
             Last_NameTextBox.Text = "";
             first_NameTextBox.Text = "";
             middle_NameTextBox.Text = "";
-           // comboSex.Text = ""; comboAge.Text = ""; mailing_AddressTextBox.Text = ""; mailing_Address_Phone_NumberTextBox.Text = ""; cityTextBox.Text = ""; city_Phone_NumberTextBox.Text = ""; citizenshipTextBox.Text = ""; religionTextBox.Text = ""; birthDateDateTimePicker.Text = ""; birthPlaceTextBox.Text = ""; comboStatus.Text = ""; guardians_NameTextBox.Text = ""; guardians_Phone_NumberTextBox.Text = ""; parents_NameTextBox.Text = ""; parents_Phone_NumberTextBox.Text = ""; streetTextBox.Text = ""; barangayTextBox.Text = ""; city_AddressTextBox.Text = ""; provinceTextBox.Text = ""; school_Name_In_ElementaryTextBox.Text = ""; date_Graduated_in_ElementaryDateTimePicker.Text = ""; school_Name_In_SecondaryTextBox.Text = ""; date_Graduated_In_SecondaryDateTimePicker.Text = ""; school_Name_In_CollegeTextBox.Text = ""; date_Graduated_In_CollegeDateTimePicker.Text = ""; school_Name_In_College_Course_TakenTextBox.Text = ""; date_Graduated_In_Course_TakenDateTimePicker.Text = ""; schoolName_1TextBox.Text = ""; courseEnrolled_1TextBox.Text = ""; semester1TextBox.Text = ""; schoolName2TextBox.Text = ""; courseEnrolled2TextBox.Text = ""; semester2TextBox.Text = ""; schoolName3TextBox.Text = ""; courseEnrolled3TextBox.Text = ""; semester3TextBox.Text = ""; organizationName1TextBox.Text = ""; positionHeld1TextBox.Text = ""; term1TextBox.Text = ""; organizationName2TextBox.Text = ""; positionHeld2TextBox.Text = ""; term2TextBox.Text = ""; organizationName3TextBox.Text = ""; positionHeld3TextBox.Text = ""; term3TextBox.Text = ""; activityTItle1TextBox.Text = ""; sponsor1TextBox.Text = ""; inclusiveDate1DateTimePicker.Text = ""; activityTitle2TextBox.Text = ""; sponsor2TextBox = ""; inclusiveDate2DateTimePicker.Text = ""; activityTitle3TextBox.Text = ""; sponsor3TextBox.Text = ""; inclusiveDate3DateTimePicker.Text = "";
+            comboSex.Text = ""; 
+            comboAge.Text = "";
+            mailing_AddressTextBox.Text = ""; 
+            mailing_Address_Phone_NumberTextBox.Text = ""; 
+            cityTextBox.Text = "";
+            city_Phone_NumberTextBox.Text = "";
+            citizenshipTextBox.Text = ""; 
+            religionTextBox.Text = ""; 
+            birthDateDateTimePicker.Text = ""; 
+            birthPlaceTextBox.Text = "";
+            comboStatus.Text = "";
+            guardians_NameTextBox.Text = ""; 
+            guardians_Phone_NumberTextBox.Text = "";
+            parents_NameTextBox.Text = ""; parents_Phone_NumberTextBox.Text = "";
+            streetTextBox.Text = "";
+            barangayTextBox.Text = "";
+            city_AddressTextBox.Text = ""; 
+            provinceTextBox.Text = ""; 
+            school_Name_In_ElementaryTextBox.Text = ""; 
+            date_Graduated_in_ElementaryDateTimePicker.Text = "";
+            school_Name_In_SecondaryTextBox.Text = ""; 
+            date_Graduated_In_SecondaryDateTimePicker.Text = "";
+            school_Name_In_CollegeTextBox.Text = ""; 
+            date_Graduated_In_CollegeDateTimePicker.Text = "";
+            school_Name_In_College_Course_TakenTextBox.Text = ""; 
+            date_Graduated_In_Course_TakenDateTimePicker.Text = "";
+            schoolName_1TextBox.Text = "";
+            courseEnrolled_1TextBox.Text = "";
+            semester1TextBox.Text = ""; 
+            schoolName2TextBox.Text = ""; 
+            courseEnrolled2TextBox.Text = "";
+            semester2TextBox.Text = ""; 
+            schoolName3TextBox.Text = ""; 
+            courseEnrolled3TextBox.Text = ""; 
+            semester3TextBox.Text = "";
+            organizationName1TextBox.Text = "";
+            positionHeld1TextBox.Text = "";
+            term1TextBox.Text = "";
+            organizationName2TextBox.Text = ""; 
+            positionHeld2TextBox.Text = ""; term2TextBox.Text = ""; 
+            organizationName3TextBox.Text = ""; 
+            positionHeld3TextBox.Text = "";
+            term3TextBox.Text = ""; 
+            activityTItle1TextBox.Text = "";
+            sponsor1TextBox.Text = ""; 
+            inclusiveDate1DateTimePicker.Text = ""; 
+            activityTitle2TextBox.Text = ""; 
+            sponsor2TextBox.Text = ""; 
+            inclusiveDate2DateTimePicker.Text = ""; 
+            activityTitle3TextBox.Text = ""; sponsor3TextBox.Text = ""; 
+            inclusiveDate3DateTimePicker.Text = "";
         
         }
 
         private void buttonDelete_Click(object sender, EventArgs e)
         {
-            int rowindex = dataGridViewStudent.CurrentRow.Index;
-            DataGridViewRow row = dataGridViewStudent.Rows[rowindex];
-            int Student_ID = Convert.ToInt32(row.Cells["Student_ID"].Value);
-
+            
             con.Open();
             OleDbCommand cmd = con.CreateCommand();
             cmd.CommandType = CommandType.Text;
-            cmd.CommandText = "delete from Student where Student_ID=" + Student_ID;
+            cmd.CommandText = "delete from Student where Student_ID=" + getStudentID();
             cmd.ExecuteNonQuery();
             con.Close();
             MessageBox.Show("Record Dnsedeletedrted Successfully", "Student Information", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
             SelectAll();
+        }
+
+        public int getStudentID() {
+            int rowindex = dataGridViewStudent.CurrentRow.Index;
+            DataGridViewRow row = dataGridViewStudent.Rows[rowindex];
+            int Student_ID = Convert.ToInt32(row.Cells["Student_ID"].Value);
+            return Student_ID;
+        }
+
+
+        //Edit
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int rowindex = dataGridViewStudent.CurrentRow.Index;
+            DataGridViewRow row = dataGridViewStudent.Rows[rowindex];
+
+            birthDateDateTimePicker.Value = Convert.ToDateTime(row.Cells["BirthDate"].Value);
+
+            
         }
 
 
