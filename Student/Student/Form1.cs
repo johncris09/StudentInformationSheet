289a33766dc7b87ed16a64fcee4ef0f79298f8a9
Johncris <jcmanabo@gmail.com>
2019-02-07 15:12:45 +0800

    try and catch

--- Student/Student/Form1.cs
+++ Student/Student/Form1.cs
@@ -34,12 +34,8 @@ namespace Student
 
             for (int age = 1; age <= 50; age++ )
             {
-                comboAge.Items.Add(age);
-
-
-            }
-
-
+                comboAge.Items.Add(age); 
+            } 
             IDGenerator();
             
         }
@@ -64,19 +60,25 @@ namespace Student
 
         private void buttonSave_Click(object sender, EventArgs e)
         {
-           
-            con.Open();
-            OleDbCommand cmd = con.CreateCommand();
-            cmd.CommandType = CommandType.Text;
-            cmd.CommandText = "Insert into Student  values('" + student_IDTextBox.Text + "','" + Last_NameTextBox.Text + "','" + first_NameTextBox.Text + "','" + middle_NameTextBox.Text + "','" + comboSex.Text + "'," + comboAge.Text + ",'" + mailing_AddressTextBox.Text + "','" + mailing_Address_Phone_NumberTextBox.Text + "','" + cityTextBox.Text + "','" + city_Phone_NumberTextBox.Text + "','" + citizenshipTextBox.Text + "','" + religionTextBox.Text + "','" + birthDateDateTimePicker.Text + "','" + birthPlaceTextBox.Text + "','" + comboStatus.Text + "','" + guardians_NameTextBox.Text + "','" + guardians_Phone_NumberTextBox.Text + "','" + parents_NameTextBox.Text + "','" + parents_Phone_NumberTextBox.Text + "','" + streetTextBox.Text + "' ,'" + barangayTextBox.Text + "','" + city_AddressTextBox.Text + "','" + provinceTextBox.Text + "' ,'" + school_Name_In_ElementaryTextBox.Text + "' ,'" + date_Graduated_in_ElementaryDateTimePicker.Text + "','" + school_Name_In_SecondaryTextBox.Text + "' ,'" + date_Graduated_In_SecondaryDateTimePicker.Text + "'  ,'" + school_Name_In_CollegeTextBox.Text + "','" + date_Graduated_In_CollegeDateTimePicker.Text + "' ,'" + school_Name_In_College_Course_TakenTextBox.Text + "' ,'" + date_Graduated_In_Course_TakenDateTimePicker.Text + "','" + schoolName_1TextBox.Text + "' ,'" + courseEnrolled_1TextBox.Text + "','" + semester1TextBox.Text + "','" + schoolName2TextBox.Text + "','" + courseEnrolled2TextBox.Text + "','" + semester2TextBox.Text + "','" + schoolName3TextBox.Text + "','" + courseEnrolled3TextBox.Text + "','" + semester3TextBox.Text + "','" + organizationName1TextBox.Text + "','" + positionHeld1TextBox.Text + "','" + term1TextBox.Text + "','" + organizationName2TextBox.Text + "','" + positionHeld2TextBox.Text + "','" + term2TextBox.Text + "','" + organizationName3TextBox.Text + "' ,'" + positionHeld3TextBox.Text + "','" + term3TextBox.Text + "','" + activityTItle1TextBox.Text + "','" + sponsor1TextBox.Text + "','" + inclusiveDate1DateTimePicker.Text + "','" + activityTitle2TextBox.Text + "','" + sponsor2TextBox + "' ,'" + inclusiveDate2DateTimePicker.Text + "' ,'" + activityTitle3TextBox.Text + "','" + sponsor3TextBox.Text + "','" + inclusiveDate3DateTimePicker.Text + "')"; //,'" + ____ + "'
-            cmd.ExecuteNonQuery();
-            con.Close();
-            MessageBox.Show("Record Inserted Successfully", "Student Information", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
-            SelectAll();
-            IDGenerator();
-            ClearAll();
-              
+            try
+            {
+                con.Open();
+                OleDbCommand cmd = con.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "Insert into Student  values('" + student_IDTextBox.Text + "','" + Last_NameTextBox.Text + "','" + first_NameTextBox.Text + "','" + middle_NameTextBox.Text + "','" + comboSex.Text + "'," + comboAge.Text + ",'" + mailing_AddressTextBox.Text + "','" + mailing_Address_Phone_NumberTextBox.Text + "','" + cityTextBox.Text + "','" + city_Phone_NumberTextBox.Text + "','" + citizenshipTextBox.Text + "','" + religionTextBox.Text + "','" + birthDateDateTimePicker.Text + "','" + birthPlaceTextBox.Text + "','" + comboStatus.Text + "','" + guardians_NameTextBox.Text + "','" + guardians_Phone_NumberTextBox.Text + "','" + parents_NameTextBox.Text + "','" + parents_Phone_NumberTextBox.Text + "','" + streetTextBox.Text + "' ,'" + barangayTextBox.Text + "','" + city_AddressTextBox.Text + "','" + provinceTextBox.Text + "' ,'" + school_Name_In_ElementaryTextBox.Text + "' ,'" + date_Graduated_in_ElementaryDateTimePicker.Text + "','" + school_Name_In_SecondaryTextBox.Text + "' ,'" + date_Graduated_In_SecondaryDateTimePicker.Text + "'  ,'" + school_Name_In_CollegeTextBox.Text + "','" + date_Graduated_In_CollegeDateTimePicker.Text + "' ,'" + school_Name_In_College_Course_TakenTextBox.Text + "' ,'" + date_Graduated_In_Course_TakenDateTimePicker.Text + "','" + schoolName_1TextBox.Text + "' ,'" + courseEnrolled_1TextBox.Text + "','" + semester1TextBox.Text + "','" + schoolName2TextBox.Text + "','" + courseEnrolled2TextBox.Text + "','" + semester2TextBox.Text + "','" + schoolName3TextBox.Text + "','" + courseEnrolled3TextBox.Text + "','" + semester3TextBox.Text + "','" + organizationName1TextBox.Text + "','" + positionHeld1TextBox.Text + "','" + term1TextBox.Text + "','" + organizationName2TextBox.Text + "','" + positionHeld2TextBox.Text + "','" + term2TextBox.Text + "','" + organizationName3TextBox.Text + "' ,'" + positionHeld3TextBox.Text + "','" + term3TextBox.Text + "','" + activityTItle1TextBox.Text + "','" + sponsor1TextBox.Text + "','" + inclusiveDate1DateTimePicker.Text + "','" + activityTitle2TextBox.Text + "','" + sponsor2TextBox + "' ,'" + inclusiveDate2DateTimePicker.Text + "' ,'" + activityTitle3TextBox.Text + "','" + sponsor3TextBox.Text + "','" + inclusiveDate3DateTimePicker.Text + "')"; //,'" + ____ + "'
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Record Inserted Successfully", "Student Information", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+                SelectAll();
+                IDGenerator();
+                ClearAll();
 
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
         }
 
         public void ClearAll()
@@ -240,8 +242,6 @@ namespace Student
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
-
-            //int count = 0;
             con.Open();
             OleDbCommand cmd = con.CreateCommand();
             cmd.CommandType = CommandType.Text;
@@ -250,30 +250,27 @@ namespace Student
             DataTable dt = new DataTable();
             OleDbDataAdapter da = new OleDbDataAdapter(cmd);
             da.Fill(dt);
-            //count = Convert.ToInt32(dt.Rows.Count.ToString());
             dataGridViewStudent.DataSource = dt;
             con.Close();
-            /*
-            if (count == 0)
-            {
-                MessageBox.Show("reocrd not found");
-            }
-             */
         }
 
         private void buttonUpdate_Click(object sender, EventArgs e)
         {
+            try { 
+                con.Open();
+                OleDbCommand cmd = con.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "Update Student Set Last_Name ='" + Last_NameTextBox.Text  +"' , First_Name ='" + first_NameTextBox.Text  +"' , Middle_Name ='" + middle_NameTextBox.Text  +"' ,Sex ='" + comboSex.Text  +"' ,Age ='" +comboAge.Text  +"' ,Mailing_Address ='" + mailing_AddressTextBox.Text +"' ,Mailing_Address_Phone_Number ='" + mailing_Address_Phone_NumberTextBox.Text  +"' , City ='" + cityTextBox.Text  +"' ,  City_Phone_Number ='" + city_Phone_NumberTextBox.Text +"' ,  Citizenship ='" + citizenshipTextBox.Text  +"' ,Religion ='" + religionTextBox.Text +"' ,  BirthDate ='" + birthDateDateTimePicker.Text  +"' ,  BirthPlace ='" + birthPlaceTextBox.Text  +"' ,  Status ='" + comboStatus.Text  +"' , Guardians_Name ='" + guardians_NameTextBox.Text  +"' , Guardians_Phone_Number ='" + guardians_Phone_NumberTextBox.Text  +"' ,Parents_Name ='" + parents_NameTextBox.Text  +"' , Parents_Phone_Number ='" + parents_Phone_NumberTextBox.Text  +"' ,Street ='" + streetTextBox.Text +"' ,Barangay ='" + barangayTextBox.Text  +"' ,City_Address ='" + city_AddressTextBox.Text  +"' , Province ='" + provinceTextBox.Text  +"' ,School_Name_In_Elementary ='" + school_Name_In_ElementaryTextBox.Text  +"' ,Date_Graduated_in_Elementary ='" + date_Graduated_in_ElementaryDateTimePicker.Text  +"' ,School_Name_In_Secondary ='" + school_Name_In_SecondaryTextBox.Text +"' ,Date_Graduated_In_Secondary ='" + date_Graduated_In_SecondaryDateTimePicker.Text  +"' ,School_Name_In_College ='" + school_Name_In_CollegeTextBox.Text  +"' ,Date_Graduated_In_College ='" + date_Graduated_In_CollegeDateTimePicker.Text  +"' ,  School_Name_In_College_Course_Taken ='" + school_Name_In_College_Course_TakenTextBox.Text  +"' ,Date_Graduated_In_Course_Taken ='" + date_Graduated_In_Course_TakenDateTimePicker.Text  +"' ,SchoolName_1 ='" + schoolName_1TextBox.Text  +"' ,CourseEnrolled_1 ='" + courseEnrolled_1TextBox.Text  +"' ,  Semester1 ='" + semester1TextBox.Text  +"' ,  SchoolName2 ='" + schoolName2TextBox.Text  +"' , CourseEnrolled2 ='" + courseEnrolled2TextBox.Text  +"' , Semester2 ='" + semester2TextBox.Text  +"' ,  SchoolName3 ='" + schoolName3TextBox.Text  +"' , CourseEnrolled3 ='" + courseEnrolled3TextBox.Text  +"' , Semester3 ='" + semester3TextBox.Text  +"' ,  OrganizationName1 ='" + organizationName1TextBox.Text  +"' , PositionHeld1 ='" + positionHeld1TextBox.Text  +"' , Term1 ='" + term1TextBox.Text  +"' , OrganizationName2 ='" + organizationName2TextBox.Text  +"' , PositionHeld2 ='" + positionHeld2TextBox.Text  +"' , Term2 ='" +term2TextBox.Text  +"' , OrganizationName3 ='" +organizationName3TextBox.Text  +"' , PositionHeld3 ='" + positionHeld3TextBox.Text  +"' , Term3 ='" + term3TextBox.Text +"' , ActivityTItle1 ='" + activityTItle1TextBox.Text +"' , Sponsor1 ='" + sponsor1TextBox.Text  +"' , InclusiveDate1 ='" + inclusiveDate1DateTimePicker.Text  +"' , ActivityTitle2 ='" + activityTitle2TextBox.Text  +"' , Sponsor2 ='" + sponsor2TextBox.Text  +"' , InclusiveDate2 ='" + inclusiveDate2DateTimePicker.Text  +"' , ActivityTitle3 ='" + activityTItle1TextBox.Text  +"' , Sponsor3 ='" + sponsor3TextBox.Text  +"' , InclusiveDate3 ='" + inclusiveDate3DateTimePicker.Text  +"' where Student_ID=" + getStudentID();
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Record Updated Successfully", "Student Information", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+                SelectAll();
+                buttonUpdate.Hide();
+                buttonSave.Show();
+            }catch(Exception ex){
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
             
-            con.Open();
-            OleDbCommand cmd = con.CreateCommand();
-            cmd.CommandType = CommandType.Text;
-            cmd.CommandText = "Update Student Set Last_Name ='" + Last_NameTextBox.Text  +"' , First_Name ='" + first_NameTextBox.Text  +"' , Middle_Name ='" + middle_NameTextBox.Text  +"' ,Sex ='" + comboSex.Text  +"' ,Age ='" +comboAge.Text  +"' ,Mailing_Address ='" + mailing_AddressTextBox.Text +"' ,Mailing_Address_Phone_Number ='" + mailing_Address_Phone_NumberTextBox.Text  +"' , City ='" + cityTextBox.Text  +"' ,  City_Phone_Number ='" + city_Phone_NumberTextBox.Text +"' ,  Citizenship ='" + citizenshipTextBox.Text  +"' ,Religion ='" + religionTextBox.Text +"' ,  BirthDate ='" + birthDateDateTimePicker.Text  +"' ,  BirthPlace ='" + birthPlaceTextBox.Text  +"' ,  Status ='" + comboStatus.Text  +"' , Guardians_Name ='" + guardians_NameTextBox.Text  +"' , Guardians_Phone_Number ='" + guardians_Phone_NumberTextBox.Text  +"' ,Parents_Name ='" + parents_NameTextBox.Text  +"' , Parents_Phone_Number ='" + parents_Phone_NumberTextBox.Text  +"' ,Street ='" + streetTextBox.Text +"' ,Barangay ='" + barangayTextBox.Text  +"' ,City_Address ='" + city_AddressTextBox.Text  +"' , Province ='" + provinceTextBox.Text  +"' ,School_Name_In_Elementary ='" + school_Name_In_ElementaryTextBox.Text  +"' ,Date_Graduated_in_Elementary ='" + date_Graduated_in_ElementaryDateTimePicker.Text  +"' ,School_Name_In_Secondary ='" + school_Name_In_SecondaryTextBox.Text +"' ,Date_Graduated_In_Secondary ='" + date_Graduated_In_SecondaryDateTimePicker.Text  +"' ,School_Name_In_College ='" + school_Name_In_CollegeTextBox.Text  +"' ,Date_Graduated_In_College ='" + date_Graduated_In_CollegeDateTimePicker.Text  +"' ,  School_Name_In_College_Course_Taken ='" + school_Name_In_College_Course_TakenTextBox.Text  +"' ,Date_Graduated_In_Course_Taken ='" + date_Graduated_In_Course_TakenDateTimePicker.Text  +"' ,SchoolName_1 ='" + schoolName_1TextBox.Text  +"' ,CourseEnrolled_1 ='" + courseEnrolled_1TextBox.Text  +"' ,  Semester1 ='" + semester1TextBox.Text  +"' ,  SchoolName2 ='" + schoolName2TextBox.Text  +"' , CourseEnrolled2 ='" + courseEnrolled2TextBox.Text  +"' , Semester2 ='" + semester2TextBox.Text  +"' ,  SchoolName3 ='" + schoolName3TextBox.Text  +"' , CourseEnrolled3 ='" + courseEnrolled3TextBox.Text  +"' , Semester3 ='" + semester3TextBox.Text  +"' ,  OrganizationName1 ='" + organizationName1TextBox.Text  +"' , PositionHeld1 ='" + positionHeld1TextBox.Text  +"' , Term1 ='" + term1TextBox.Text  +"' , OrganizationName2 ='" + organizationName2TextBox.Text  +"' , PositionHeld2 ='" + positionHeld2TextBox.Text  +"' , Term2 ='" +term2TextBox.Text  +"' , OrganizationName3 ='" +organizationName3TextBox.Text  +"' , PositionHeld3 ='" + positionHeld3TextBox.Text  +"' , Term3 ='" + term3TextBox.Text +"' , ActivityTItle1 ='" + activityTItle1TextBox.Text +"' , Sponsor1 ='" + sponsor1TextBox.Text  +"' , InclusiveDate1 ='" + inclusiveDate1DateTimePicker.Text  +"' , ActivityTitle2 ='" + activityTitle2TextBox.Text  +"' , Sponsor2 ='" + sponsor2TextBox.Text  +"' , InclusiveDate2 ='" + inclusiveDate2DateTimePicker.Text  +"' , ActivityTitle3 ='" + activityTItle1TextBox.Text  +"' , Sponsor3 ='" + sponsor3TextBox.Text  +"' , InclusiveDate3 ='" + inclusiveDate3DateTimePicker.Text  +"' where Student_ID=" + getStudentID();
-            cmd.ExecuteNonQuery();
-            con.Close();
-            MessageBox.Show("Record Updated Successfully", "Student Information", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
-            SelectAll();
-            buttonUpdate.Hide();
-            buttonSave.Show();
         }
 
 
